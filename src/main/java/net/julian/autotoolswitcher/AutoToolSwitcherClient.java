cf80232e4831101d1d962ccaee3370c00a3bda1f
Julian <jc.julian.carpenter@gmail.com>
2023-04-06 22:33:31 -0500

    Initial commit

---
+++ src/main/java/net/julian/autotoolswitcher/AutoToolSwitcherClient.java
@@ -0,0 +1,10 @@
+package net.julian.autotoolswitcher;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoToolSwitcherClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
